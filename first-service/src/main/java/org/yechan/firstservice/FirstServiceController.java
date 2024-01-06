b3222873110efc8b1049be0275dbe8f5bfb6cd20
YeaChan05 <qkenrdl05@gmail.com>
2024-01-06 23:51:31 +0900

    Spring Cloud Gateway - Load Balancer

--- first-service/src/main/java/org/yechan/firstservice/FirstServiceController.java
+++ first-service/src/main/java/org/yechan/firstservice/FirstServiceController.java
@@ -1,6 +1,9 @@
 package org.yechan.firstservice;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
+    @Autowired
+    Environment env;
+    public FirstServiceController(Environment env){
+        this.env = env;
+    }
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First service.";
@@ -21,7 +29,8 @@ public class FirstServiceController {
     }
     
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request){
+        log.info("Server port={}",request.getServerPort());
+        return String.format("Hi, there. This is a message from First Service on Port %s.",env.getProperty("local.server.port"));
     }
 }
