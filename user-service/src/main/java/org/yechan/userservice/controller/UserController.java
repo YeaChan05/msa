ea06507239f19ffa07c8c6c98526557be28e1c9c
YeaChan05 <qkenrdl05@gmail.com>
2024-01-11 15:36:38 +0900

    Config Microservice - local repository configuration

--- user-service/src/main/java/org/yechan/userservice/controller/UserController.java
+++ user-service/src/main/java/org/yechan/userservice/controller/UserController.java
@@ -25,17 +25,22 @@ public class UserController {
     
     @Autowired
     private Greeting greeting;
+    
     @Autowired
     
-    public UserController(Environment env,UserService userService) {
+    public UserController(Environment env, UserService userService) {
         this.env = env;
-        this.userService=userService;
+        this.userService = userService;
     }
     
     @GetMapping("/health_check")
     public String status() {
-        return String.format("It's Working in User Service on PORT %s",
-                env.getProperty("local.server.port"));
+        return String.format("It's Working in User Service"
+                + ", port(local.sever.port)=" + env.getProperty("local.server.port")
+                + ", port(sever.port)=" + env.getProperty("server.port")
+                + ", token secret=" + env.getProperty("token.secret")
+                + ", token expiration time=" + env.getProperty("token.expiration_time")
+        );
     }
     
     @GetMapping("/welcome")
@@ -45,34 +50,34 @@ public class UserController {
     }
     
     @PostMapping("/users")
-    public ResponseEntity createUser(@RequestBody RequestUser user){
-        ModelMapper mapper=new ModelMapper();
+    public ResponseEntity createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         
-        UserDto userDto=mapper.map(user,UserDto.class);
+        UserDto userDto = mapper.map(user, UserDto.class);
         userService.createUser(userDto);
         
-        ResponseUser responseUser=mapper.map(userDto, ResponseUser.class);
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
         
         return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
     }
     
     @GetMapping("/users")
-    public ResponseEntity<List<ResponseUser>> getUsers(){
-        Iterable<UserEntity> userList= userService.getUserByAll();
+    public ResponseEntity<List<ResponseUser>> getUsers() {
+        Iterable<UserEntity> userList = userService.getUserByAll();
         
-        List<ResponseUser> result=new ArrayList<>();
-        userList.forEach(v->{
-            result.add(new ModelMapper().map(v,ResponseUser.class));
+        List<ResponseUser> result = new ArrayList<>();
+        userList.forEach(v -> {
+            result.add(new ModelMapper().map(v, ResponseUser.class));
         });
         
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
     
     @GetMapping("/users/{userId}")
-    public ResponseEntity<ResponseUser> getUser(@PathVariable String userId){
+    public ResponseEntity<ResponseUser> getUser(@PathVariable String userId) {
         UserDto userDto = userService.getUserByUserId(userId);
-        ResponseUser returnValue=new ModelMapper().map(userDto,ResponseUser.class);
+        ResponseUser returnValue = new ModelMapper().map(userDto, ResponseUser.class);
         return ResponseEntity.status(HttpStatus.OK).body(returnValue);
     }
 }
